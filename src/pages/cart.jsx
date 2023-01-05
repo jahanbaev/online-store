eb5ff16737c5080b495efe5fffe1fe1a8b31161a
jahanbaev <sjahanbaev@gmail.com>
2023-01-05 20:40:03 +0500

    init: cart page

--- src/pages/cart.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Cart = () =>{
-    return <div>hello from cart</div>
-} 
-
-export default Cart
---
+++ src/pages/cart.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+interface product {
+    "id": number,
+    "title": string,
+    "description": string,
+    "price": number,
+    "discountPercentage":number,
+    "rating": number,
+    "stock": number,
+    "brand": string,
+    "category": string,
+    "thumbnail": string,
+    "images": string[]
+}
+
+const Cart = () =>{
+    const [list, settList] = useState<product[]>([]);
+
+    useEffect(() => {
+        getList()
+    }, [])
+    function getList(){
+        if(localStorage.getItem('cart') == null){
+            localStorage.setItem("cart", "[]")
+        }
+
+        let products : product[] = JSON.parse(localStorage.getItem('cart')  + "")
+        settList(products)
+    }
+    return <div>
+        {
+            list.map((e : product, index : number)=>{
+                    return (<div className="p-2" key={index} >
+                                <div className="bg-white w-full flex">
+                                    <img src={e.images[0]} className="w-32 h-28" alt="" />
+                                    <div className="p-2">
+                                    <h1 className="text-xl text-slate-900">{e.title}</h1>
+                                    <h1 className="text-xl text-slate-900">{e.description}</h1>
+                                    </div>
+                                </div>
+                            </div>) 
+            })
+            
+        }
+    </div>
+} 
+
+export default Cart
